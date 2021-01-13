acd5cebcf64c31fad472e18fc0fd7356a71def40
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-13 12:57:47 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else 
+        katz_deli.each_with_index do |customer, number|
+            queue = []
+            queue <<  "#{number + 1}. #{customer} "
+        end
+            queue.unshift("The line is currently: ")
+            puts queue.join
+    end
+end
