60c16ff6d1958458dd7633bb6b27d8a0cec247b8
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-13 13:27:49 -0800

    commit tree 5a52e8f0e729115e33d0776995d18a8ecd9a2eae

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,32 @@ def line(katz_deli)
     if katz_deli.length == 0
         puts "The line is currently empty."
     else 
+        queue = []
         katz_deli.each_with_index do |customer, number|
-            queue = []
-            queue <<  "#{number + 1}. #{customer} "
+            queue <<  " #{number + 1}. #{customer}"
         end
-            queue.unshift("The line is currently: ")
+        queue.unshift("The line is currently:")
             puts queue.join
+    end
+end
+
+def take_a_number(katz_deli, name)
+    if katz_deli.length == 0
+        katz_deli << name
+        puts "Welcome, #{name}. You are number 1 in line."
+    else
+        katz_deli << name
+        number = katz_deli.index(name)
+        puts "Welcome, #{name}. You are number #{number+ 1} in line."
+    end
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        name = katz_deli[0] 
+        puts "Currently serving #{name}."
+        katz_deli.shift
     end
 end
